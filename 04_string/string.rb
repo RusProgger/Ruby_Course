b6707e46fa207b486c50d075c2d64a4c88480a60
RusProgger <ruslantimka92@gmail.com>
2026-07-14 00:32:16 +0300

    Add lesson string

---
+++ 04_string/string.rb
@@ -0,0 +1,21 @@
+# строки
+
+name = "Alex"
+
+puts "Имя: #{name}"
+
+
+name = "Ivan"
+
+puts "Имя: #{name}"
+
+NAMES  = "Denis".freeze
+
+# NAMES << "Alexey" # ошибка 
+
+puts "Имя: #{NAMES}"
+
+
+# Умножение строк 
+
+puts "My name: #{name}\n" * 3
