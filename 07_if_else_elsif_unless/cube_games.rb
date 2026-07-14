3ecd1e66ae2082b7a1748ed9bc264ce748716fa5
RusProgger <ruslantimka92@gmail.com>
2026-07-14 12:50:16 +0300

    Update cube_games.rb

--- 07_if_else_elsif_unless/cube_games.rb
+++ 07_if_else_elsif_unless/cube_games.rb
@@ -8,6 +8,11 @@ printf "Угадайте число: "
 
 number = gets.to_i
 
-if number == cube
-    puts "Вы угадали!"
+if number < 1 || number > 6
+    puts "Ошибка!! Введите число от 1 до 6.."
+    elsif number == cube
+        puts "Вы угадали!"    
+else
+    puts "Вы не угадали! Число было: #{cube}"
+
 end
