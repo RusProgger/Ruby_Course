9e79aaa3479fbc6c7e092da89086133391d5b3dc
RusProgger <ruslantimka92@gmail.com>
2026-07-16 14:55:43 +0300

    Add array

--- 11_array/array.rb
+++ 11_array/array.rb
@@ -5,3 +5,14 @@ user = "Алина", "Дина", "Юлия", "Лена", "Даша"
 
 puts user
 
+puts "\n\n----------------------------\n\n"
+
+# Второй способ
+
+user_person = ["Alina", "Diana", "Julia", "Elena", "Dasha"]
+
+for i in 0...user_person.length
+    puts "Вывод персонажей имен: #{user_person[i]}"
+end
+
+#puts user_person
