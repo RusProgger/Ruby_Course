1d65850d6a0f9e124185e6f3d45b9c16a2b3a36e
RusProgger <ruslantimka92@gmail.com>
2026-07-16 15:00:17 +0300

    Add array

--- 11_array/array.rb
+++ 11_array/array.rb
@@ -9,10 +9,17 @@ puts "\n\n----------------------------\n\n"
 
 # Второй способ
 
+count = 1 # счетчик
+
 user_person = ["Alina", "Diana", "Julia", "Elena", "Dasha"]
 
-for i in 0...user_person.length
-    puts "Вывод персонажей имен: #{user_person[i]}"
+# for i in 0...user_person.length
+#     puts "Вывод персонажей имен: #{user_person[i]}"
+# end
+
+for user in user_person
+    puts "Вывод персонажей #{count}: #{user}" 
+    count += 1
 end
 
 #puts user_person
